ee91f316b87019249ddd493fd97707f229ebf5ad
Kry5t14n <savcio9182@gmail.com>
2022-06-06 19:07:43 +0200

    html and css

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const port = 3000
-
 const app = express()
-app.listen(port)
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.listen(port)
